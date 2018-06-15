e5c4f10a4122359f8aedcdd361a2d392b585e5b7
siangyeh <siangyeh@emotibot.com>
2018-06-15 14:02:53 +0800

    add new scripts

--- image-build/3.2-0615/scripts/init-shard.sh
+++ image-build/3.2-0615/scripts/init-shard.sh
@@ -16,11 +16,11 @@ mongodb33=`getent hosts ${MONGO33} | awk '{ print $1 }'`
 
 port=${PORT:-27017}
 
-echo "Waiting for startup.."
-until mongo --host ${mongodb1}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
-  printf '.'
-  sleep 1
-done
+#echo "Waiting for startup.."
+#until mongo --host ${mongodb1}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
+#  printf '.'
+#  sleep 1
+#done
 
 echo "Started.."
 
