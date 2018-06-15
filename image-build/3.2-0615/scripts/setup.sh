7f555861548226c379dff62bafe2f3948f0c6624
siangyeh <siangyeh@emotibot.com>
2018-06-15 14:50:27 +0800

    add debug info to setup.sh

--- image-build/3.2-0615/scripts/setup.sh
+++ image-build/3.2-0615/scripts/setup.sh
@@ -4,7 +4,14 @@ mongodb1=`getent hosts ${MONGO1} | awk '{ print $1 }'`
 mongodb2=`getent hosts ${MONGO2} | awk '{ print $1 }'`
 mongodb3=`getent hosts ${MONGO3} | awk '{ print $1 }'`
 
+echo 'mongodb1 = ${mongodb1}'
+echo 'mongodb2 = ${mongodb2}'
+echo 'mongodb3 = ${mongodb3}'
+
 port=${PORT:-27017}
+
+echo 'port =${port}'
+echo 'id value =${RS}'
 
 #echo "Waiting for startup.."
 #until mongo --host ${mongodb1}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
